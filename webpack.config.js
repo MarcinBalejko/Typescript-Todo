f1e9082aa817ce29b423cf3196cab68fe991c2a0
MarcinBalejko <mbbm9@o2.pl>
2020-04-16 22:43:21 +0200

    :construction: Getting todos

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ module.exports = {
     path: __dirname + "/dist",
     filename: "app.bundle.js",
   },
+  resolve: {
+    extensions: ["*", "webpack.js", ".web.js", ".ts", ".js"],
+  },
   module: {
     rules: [{ test: /\.ts$/, use: "ts-loader" }],
   },
